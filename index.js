7d02305b2d86608ed75c610f5dbb778e2c3e43f6
GikuyuNderitu <gikuyunderitu@gmail.com>
2018-05-23 18:14:07 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const Koa = require('koa');
+const PORT = process.env.PORT || 1337;
+
+const app = new Koa();
+
+app.proxy =true;
+
+app.use(async (ctx, next) => {
+  if(ctx.request.path === '/favicon.ico') {
+    return;
+  }
+  next();
+})
+
+app.use(async (ctx, next) => {
+  console.log(ctx.request.ips)
+
+});
+
+app.listen(PORT, () => {
+  console.log('====================================');
+  console.log(`The server is running on port: ${PORT}`);
+  console.log('====================================');
+})
