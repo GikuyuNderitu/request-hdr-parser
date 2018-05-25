f50cfa65806385cbe44f481937b832916edc98fe
GikuyuNderitu <gikuyunderitu@gmail.com>
2018-05-25 14:31:37 -0700

    Finish header parser

--- index.js
+++ index.js
@@ -11,17 +11,63 @@ app.use(async (ctx, next) => {
     return;
   }
   next();
-})
+});
 
 app.use(async (ctx, next) => {
-  console.log(ctx.req.connection.remoteAddress)
+  const headers = Object.freeze(ctx.request.header);
 
-  ctx.body = ctx.req.connection.remoteAddress
+  const response = {
+    language: languageParse(headers),
+    ip: ctx.request.ip,
+  }
 
+  try{
+    const operating_system = await systemParse(headers);
+
+    console.log(operating_system)
+    response.operating_system = operating_system;
+  } catch (e) {
+    response.operating_system = '';
+  }
+
+
+  ctx.body = JSON.stringify(response, null, 4);
 });
 
 app.listen(PORT, () => {
   console.log('====================================');
   console.log(`The server is running on port: ${PORT}`);
   console.log('====================================');
-})
+});
+
+/**
+ * 
+ * @param {ContextDelegatedRequest.header} headers 
+ */
+const languageParse = headers => `${headers['accept-language'].split(';')[0]}`
+
+const systemParse = async headers => {
+  /** @type {string} */
+  const userAgent = headers['user-agent'];
+  if(!userAgent || !userAgent.trim()) return '';
+
+  const incrementor = increaseOrThrow(userAgent.length)
+
+  let ptr = 0;
+  while(userAgent[ptr] !== '(') ptr ++;
+  let buffer = '';
+  while (userAgent[ptr + 1] !== ')') buffer += userAgent[ptr = incrementor(ptr)];
+
+  return buffer;
+}
+
+/**
+ * 
+ * @param {number} len 
+ * @returns {function(number): number}
+ */
+const increaseOrThrow = len => num => {
+  if(num > len) throw new Error(`You have increased beyond the given length`);
+  console.log(num)
+  return num + 1;
+}
